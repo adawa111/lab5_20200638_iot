46ab748c14309824e3a6338011e5ec16c452a4d9
Alejandro Gutierrez Chavarria <113729680+adawa111@users.noreply.github.com>
2024-05-26 11:57:12 -0500

    k

--- app/src/main/java/com/example/lab5_20200638_iot/MainActivity.java
+++ app/src/main/java/com/example/lab5_20200638_iot/MainActivity.java
@@ -4,6 +4,8 @@ import static android.Manifest.permission.POST_NOTIFICATIONS;
 
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Build;
@@ -15,6 +17,7 @@ import androidx.activity.EdgeToEdge;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
 
 import java.io.Serializable;
 
@@ -82,8 +85,40 @@ public class MainActivity extends AppCompatActivity {
         String codigo =  editTextText.getText().toString().trim();
         Intent intent = new Intent(this, ListaTareas.class);
         intent.putExtra("codigo", (Serializable) codigo);
+        //createPersistentNotification(codigo);
 
 
         startActivity(intent);
+    }
+/*Con Ayuda de code Chatgpt*/
+    private void createPersistentNotification(String codigo) {
+        // Crear un Intent para abrir la actividad principal al hacer clic en la notificación
+        Intent notificationIntent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "Código PUCP";
+            String description = "Notificación persistente con el código PUCP";
+            int importance = NotificationManager.IMPORTANCE_LOW;
+            NotificationChannel channel = new NotificationChannel("channelDefaultPri", name, importance);
+            channel.setDescription(description);
+
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "channelDefaultPri")
+                .setSmallIcon(R.drawable.ic_notification)
+                .setContentTitle("Código PUCP")
+                .setContentText("Código: " + codigo)
+                .setPriority(NotificationCompat.PRIORITY_LOW)
+                .setOngoing(true)
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(false);
+
+        // Mostrar la notificación
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(1, builder.build());
     }
 }
